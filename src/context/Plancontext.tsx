3b2bdfc19052f6ef23be4b0af39a420e9dae809a
Saif-nyt <saifulislamnyt@gmail.com>
2026-09-24 11:38:41 +0600

    Context created

--- src/context/Plancontext.tsx
+++ src/context/Plancontext.tsx
@@ -1,17 +1,23 @@
 
-
-
+import {useState} from 'react'
 import React from 'react';
+const Playcontext = createContext()
+const PlanProvider = ({children}) => {
 
-const Plancontext = () => {
-
-
-    
+    const [plan, setPlan] = useState([]);
+  const [saved, setSaved] = useState([]);
+  const sharedata= {
+    plan,
+    setPlan,
+    saved,
+    setSaved
+  }
     return (
         <div>
-            
+          <Playcontext.provider value ={sharedata} >{children}</Playcontext.provider>
+        
         </div>
     );
 };
 
-export default Plancontext;
+export default PlanProvider;
